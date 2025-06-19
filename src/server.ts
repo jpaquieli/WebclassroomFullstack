975a1da7a86443231f07ee9772bc5caca02f571b
Julio Paquieli <juliopaquieli@MacBook-Air-de-Julio.local>
2025-06-19 15:09:50 -0300

    Primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { env } from './env';
+import { app } from './app';
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT
+  }, () => {
+    console.log(`Server is running on http://localhost:${env.PORT}`);
+  });
