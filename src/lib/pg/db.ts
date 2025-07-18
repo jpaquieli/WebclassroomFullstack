47d84ebb67177343ff307aabbbec720fe2c4477c
Julio Paquieli <juliopaquieli@MacBook-Air-de-Julio.local>
2025-07-18 13:49:15 -0300

    fix: Adicionando configuração de SSL no db para funcionamento no render.

--- src/lib/pg/db.ts
+++ src/lib/pg/db.ts
@@ -2,12 +2,15 @@ import { Pool, PoolClient } from 'pg';
 import 'dotenv/config';
 import { env } from '@/env';
 
+const isProduction = env.NODE_ENV === 'production';
+
 const CONFIG = {
   user: env.DATABASE_USER,
   host: env.DATABASE_HOST,
   database: env.DATABASE_NAME,
   password: env.DATABASE_PASSWORD,
   port: env.DATABASE_PORT,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 };
 
 class Database {
@@ -25,8 +28,7 @@ class Database {
       console.log('Database connected.');
     } catch (error) {
       console.error(`Error connecting to database: ${error}`);
-
-      throw new Error(`Error connecting to database:${error}`);
+      throw new Error(`Error connecting to database: ${error}`);
     }
   }
 
